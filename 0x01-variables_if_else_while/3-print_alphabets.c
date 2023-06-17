8b3df2985389edd127af51fee9ae4b1b40eda76d
SaadM33 <119438070+SaadM33@users.noreply.github.com>
2023-06-17 02:48:14 +0100

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lower and upper cases
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
