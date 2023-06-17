1400c668a26d0a3098543c9650f4c07fc6060c68
SaadM33 <119438070+SaadM33@users.noreply.github.com>
2023-06-17 02:56:25 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,13 +3,13 @@
 /**
  * main - Prints the alphabet in lower and upper cases
  *
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(void)
 {
 char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
-	for (i = 0; i < 52; i++)
+	for (i = 0; i < 52; i++) 
 	{
 		putchar(alphabet[i]);
 	}
