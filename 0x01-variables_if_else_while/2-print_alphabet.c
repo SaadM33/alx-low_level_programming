2ded433cded5765933166ce45bb8513940d40df7
SaadM33 <smaaqili@gmail.com>
2023-06-18 05:35:14 -0700

    last commit to fix untracked file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+*main - prints the alphbaet using do while loop and putchar
+*
+*Return - always 0
+*/
+int main(void)
+{
+    char c;
+
+    do
+    {   
+        putchar("%c", &c);
+
+        c++;
+
+    }while(c!=z)
+
+    return (0);
+}
