30db33ae20f00b8a2716f1163dcb74347291db5b
SaadM33 <119438070+SaadM33@users.noreply.github.com>
2023-06-22 14:26:03 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,14 +8,14 @@
 
 int _isupper(int c)
 {
-	int i = 0;
-	char tab[] = "ABCDEFGHIJKLMNOPQRSTXYZ";
+int i = 0;
+char tab[] = "ABCDEFGHIJKLMNOPQRSTXYZ";
 
-	for (i; i != '\0'; i++)
-	{
-		if (c == tab[i])
-			return (1);
-	}
-	return (0);
+for (i; i != '\0'; i++)
+{
+if (c == tab[i])
+return (1);
+}
+return (0);
 }
 
