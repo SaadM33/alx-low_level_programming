51a3e69481cfbc3bccf624778045559d21cbced3
SaadM33 <119438070+SaadM33@users.noreply.github.com>
2023-06-22 19:37:58 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,9 @@
-#include "main.h"  // Assuming "holberton.h" is the header file where _putchar is declared
+#include "main.h"
+/**
+ * print_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
 
 void print_numbers(void)
 {
