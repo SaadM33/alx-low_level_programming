b07d456724dab5b18ef30375f6837615f87e59a1
SaadM33 <smaaqili@gmail.com>
2023-06-22 06:19:56 -0700

    added _isupper fonction

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
+
+int _isupper(int c)
+{
+	int i = 0;
+	char tab[] = "ABCDEFGHIJKLMNOPQRSTXYZ";
+
+	for (i; i != '\0'; i++)
+	{
+		if (c == tab[i])
+			return (1);
+	}
+	return (0);
+}
+
