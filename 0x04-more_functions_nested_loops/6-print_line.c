fd8c2f671d6ce3c493c5009d971fbe7ba0a6e971
SaadM33 <119438070+SaadM33@users.noreply.github.com>
2023-06-22 22:18:02 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,12 +6,12 @@
 
 void print_line(int n)
 {
-	if (n > 0)
-	{
-		for (int i = 0; i < n; i++)
-		{
-		_putchar(_);
-		}
-	}
-	_putchar(\n);
+if (n > 0)
+{
+for (int i = 0; i < n; i++)
+{
+_putchar(_);
+}
+}
+_putchar(\n);
 }
