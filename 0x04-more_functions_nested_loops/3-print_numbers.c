43604f25110c5a3f2864971667ac5d090f4a4b91
SaadM33 <smaaqili@gmail.com>
2023-06-22 11:34:56 -0700

    haay

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"  // Assuming "holberton.h" is the header file where _putchar is declared
+
+void print_numbers(void)
+{
+    int num;
+
+    for (num = 0; num <= 9; num++)
+    {
+        _putchar(num + '0');
+    }
+    _putchar('\n');
+}
+
