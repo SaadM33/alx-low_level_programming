75044b18a204ab2c2a8e4c56e0a6864751e79c81
SaadM33 <119438070+SaadM33@users.noreply.github.com>
2023-06-22 14:30:00 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,20 @@
 #include "main.h"
-
 /**
 * _isupper - checks for uppercase character
 * @c: Variable text
 * Return: Always 0.
 */
-
 int _isupper(int c)
 {
-int i = 0;
-char tab[] = "ABCDEFGHIJKLMNOPQRSTXYZ";
 
-for (i; i != '\0'; i++)
+
+ if (c >= 'A' && c <= 'Z')
+ {
+ return (1);
+ }
+ else
 {
-if (c == tab[i])
-return (1);
-}
-return (0);
+ return (0);
 }
 
+}
