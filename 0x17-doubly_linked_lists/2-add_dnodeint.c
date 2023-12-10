38911fa5ad2416e671aebee716f669442e0f9723
SaadM33 <119438070+SaadM33@users.noreply.github.com>
2023-12-10 03:25:09 +0100

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,28 +2,33 @@
 #include <string.h>
 
 /**
- * *add_dnodeint - adds a node to a linked list head
- * @head: linked list head
- * @n: int element of the linked list
- * Return: number of elements
+ * add_dnodeint - adds a node to the beginning of a linked list
+ * @head: double pointer to the head of the linked list
+ * @n: integer element to be added
+ * Return: pointer to the new node or NULL on failure
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+    dlistint_t *new_node;
 
-	new_node = malloc(sizeof(dlistint_t));
+    new_node = malloc(sizeof(dlistint_t));
 
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+    if (new_node == NULL)
+    {
+        return (NULL);  // No need to free(new_node) before returning NULL
+    }
 
-	new_node->n = n;
-	new_node->next = *head;
+    new_node->n = n;
+    new_node->next = *head;
     new_node->prev = NULL;
-	**head = new_node;
 
-	return (new_node);
+    if (*head != NULL)
+    {
+        (*head)->prev = new_node;
+    }
+
+    *head = new_node;
+
+    return (new_node);
 }
