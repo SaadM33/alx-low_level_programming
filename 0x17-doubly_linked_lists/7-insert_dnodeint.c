715a5751a63f6b0ca1143cc1edf58dcdeba999a1
SaadM33 <119438070+SaadM33@users.noreply.github.com>
2023-12-17 03:32:07 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,37 +10,39 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current;
-	dlistint_t *new;
-	int i = 0;
+    dlistint_t *current;
+    dlistint_t *new;
+    unsigned int i = 0;
 
-	if (h == NULL)
-		return (0);
+    if (h == NULL)
+        return (NULL);
 
-	if (idx == 0)
-        	return (add_dnodeint(h, n));
+    if (idx == 0)
+        return (add_dnodeint(h, n));
 
-	current = *h;
+    current = *h;
 
-	while (i < idx - 1)
-	{
-		current = current->next;
-		i++;
-		if (current == NULL)
-			return (NULL);
-	}
+    while (i < idx - 1)
+    {
+        current = current->next;
+        i++;
+        if (current == NULL)
+            return (NULL);
+    }
 
-	new = malloc(sizeof(dlistint_t));
+    new = malloc(sizeof(dlistint_t));
 
-	if (new == NULL)
-		return (NULL);
+    if (new == NULL)
+        return (NULL);
 
-	new->n = n;
-	new->next = current->next;
-	new->prev = current;
+    new->n = n;
+    new->next = current->next;
+    new->prev = current;
 
-	if (current->next != NULL)
-        	current->next->prev = new;
+    if (current->next != NULL)
+        current->next->prev = new;
 
-	return (new);
+    current->next = new;
+
+    return (new);
 }
